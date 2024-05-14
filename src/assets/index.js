790c664fb071f413a26124f4eaada6f86116d04e
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-05-14 20:30:06 +0200

    adding-LaUIX

---
+++ src/assets/index.js
@@ -0,0 +1,26 @@
+import homeHero from "./home_hero.png";
+import homeCards from "./home_cards.png";
+import feature from "./feature.png";
+import expo from "./expo.png";
+import expo02 from "./expo02.png";
+import javascript from "./javascript.png";
+import react from "./react.png";
+import scene from "./scene.png";
+import mockup from "./mockup.png";
+import nodejs from './node-js-svgrepo-com.svg'
+import ts from './typescript-svgrepo-com.svg'
+import postman from './postman-icon-svgrepo-com.svg'
+export default {
+  homeHero,
+  homeCards,
+  feature,
+  expo,
+  expo02,
+  javascript,
+  nodejs,
+  postman,
+  ts,
+  react,
+  scene,
+  mockup,
+};
